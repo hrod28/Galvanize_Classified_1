72f71443f7bd17d8046f4eaf6954186e44943016
Heidi Rodriguez <hrod2645@gmail.com>
2017-01-30 09:50:00 -0700

    get route passing

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,19 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex');
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/', (req, res, next)=>{
+  knex('classifieds')
+    .select('id', 'title', 'description', 'price', 'item_image')
+    .orderBy('id', 'asc')
+    .then((results)=>{
+      res.json(results);
+    })
+    .catch((err)=>{
+      res.send(err)
+    })
+}); 
 
 module.exports = router;
